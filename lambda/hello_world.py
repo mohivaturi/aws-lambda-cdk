60c741703ceadb99e29b6e1380caf8a558c0721f
mohivaturi <ivaturimohith7@gmail.com>
2025-03-10 16:20:15 -0400

    fixes done

--- lambda/hello_world.py
+++ lambda/hello_world.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": json.dumps("Hello, World from AWS Lambda! from github actions")
+        "body": json.dumps("Hello, World from AWS Lambda from github actions!")
     }
